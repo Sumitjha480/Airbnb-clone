06b09ad7a9a4a7e0cbd1c2e16b3038b5bb9f500c
Sumit <sumitjha480@gmail.com>
2022-10-31 14:49:53 +0530

    Initial build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com", "www.unsplash.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYW1pbGxzMjUiLCJhIjoiY2w5cmdkczNhMGZ3ajNucGtjNzVvb3lzMCJ9.lC4vtaCMVlsA-aL_XKRhOQ",
+  },
 }
